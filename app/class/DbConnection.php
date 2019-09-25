bc6c9569cdddf9c27af45060726c929873fe6472
Michael I. Zubi <mzubi@indiana.edu>
2019-09-24 21:03:41 -0400

    I made a mistake

--- app/class/DbConnection.php
+++ app/class/DbConnection.php
@@ -14,12 +14,12 @@ class DbConnection
       }
 
       try {
-          $dsn = 'mysql:host='.$_ENV['mzubi-msis-ds0923.clkefstulqsl.us-east-1.rds.amazonaws.com'].';dbname='.$_ENV['mzubi-msis-ds0923'].';charset=utf8';
+          $dsn = 'mysql:host='.$_ENV['MYSQL_HOST'].';dbname='.$_ENV['MYSQL_DATABASE'].';charset=utf8';
           error_log($dsn);
           self::$connection = new PDO(
              $dsn,
-             $_ENV['admin'],
-             $_ENV['W1!8hFv&j!I&X7T'],
+             $_ENV['MYSQL_USER'],
+             $_ENV['MYSQL_PASSWORD'],
              [
                  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
