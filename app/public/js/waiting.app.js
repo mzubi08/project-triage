459a130ef1afce7403bacc5f66b3c6c156f190ac
Michael I. Zubi <mzubi@indiana.edu>
2019-10-14 11:07:19 -0400

    here

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -8,6 +8,12 @@ var waitingApp = new Vue({
       fetch('api/waiting/')
       .then(response => response.json())
       .then(json => { waitingApp.patients = json })
+    },
+    displayWaitingLocalDate(d) {
+      return moment.utc(d).local().format("kk:mm, M-D");
+    },
+    displayWaitingSince(d){
+      return moment.utc(d).local().fromNow();
     }
   },
   created() {
